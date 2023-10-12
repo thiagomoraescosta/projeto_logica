844e5a1ff9b6a4800a121eebf4ddaf719449ca57
Thiago Moraes <133033871+thiagomoraescosta@users.noreply.github.com>
2023-10-12 17:37:59 -0300

    atualizei o desafio de logica.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("olá Mundo")
+let nomeHeroi = "GoKu";
+let classificacaoXp = 10001;
+let nivel1 = 1000;
+let nivel2 = 2000;
+let nivel3 = 5000;
+let nivel4 = 7000;
+let nivel5 = 8000;
+let nivel6 = 9000;
+let nivel7 = 10000;
+
+
+if (classificacaoXp <= nivel1) {
+    classificacaoXp = "Forte";
+}
+
+if (classificacaoXp <= nivel2){
+    classificacaoXp = "Bronze";
+}
+
+if (classificacaoXp <= nivel3){
+    classificacaoXp = "Prata"
+}
+
+
+if (classificacaoXp <= nivel4){
+    classificacaoXp = "Ouro"
+}
+
+if (classificacaoXp <= nivel5){
+    classificacaoXp = "Platina"
+}
+
+if (classificacaoXp <= nivel6){
+    classificacaoXp = "Ascendente"
+}
+
+if (classificacaoXp <= nivel7){
+    classificacaoXp = "Imortal"
+}
+
+if (classificacaoXp > nivel7){
+    classificacaoXp = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " esta no nível de " + classificacaoXp);
