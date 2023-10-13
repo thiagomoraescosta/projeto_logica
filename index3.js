0be8f1aa589d3fe99d2ec03ea11b918972cc9c35
Thiago Moraes <133033871+thiagomoraescosta@users.noreply.github.com>
2023-10-13 14:08:41 -0300

    outra forma de resolver o projeto

---
+++ index3.js
@@ -0,0 +1,22 @@
+let nomeHeroi = "GoKu";
+let xp = 10001;
+
+   if (xp <= 1000){
+    novoXp = "Forte";  
+   } else if (xp <= 2000){
+    novoXp = "Bronze";
+   } else if (xp <= 5000){
+    novoXp = "Prata"; 
+   } else if (xp <= 7000){
+    novoXp = "Ouro"; 
+   } else if (xp <= 8000){
+    novoXp = "Platina"; 
+   } else if (xp <= 9000){
+    novoXp = "Ascendente"; 
+   } else if (xp <= 10000){
+    novoXp = "Imortal"; 
+   } else {
+    novoXp = "Radiante"; 
+   } 
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + novoXp);
